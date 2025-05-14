03e45295d7f70f1d2bf09f89fa1ae95f995a1a56
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-14 13:24:00 -0500

    feat(fortune): add fortune cookie feature with view, template, and URL
    This commit introduces a new fortune cookie feature. It includes:
    - A new URL route `/fortune/` mapped to the `fortune` view.
    - A `fortune` view that randomly selects a fortune from a predefined list and passes it to the template.
    - A new `fortune.html` template that displays the selected fortune and provides a link to return to the home page.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import random
+
+fortune_list = ["You will have a great day!", "You will meet a new friend.", "Will be rich in the near future.", "You will have a fantastic career.", "You will have a great life."]
 
 # Create your views here.
 def home(request):
     return render(request, 'myapp/home.html')
+
+def fortune(request):
+    context = {'fortune': random.choice(fortune_list)}
+    return render(request, 'myapp/fortune.html', context)
